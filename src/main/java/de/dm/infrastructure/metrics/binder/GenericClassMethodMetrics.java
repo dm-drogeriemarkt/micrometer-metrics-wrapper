1b053be4f2e822af96beb30e8609afbd40670bfb
Fessele, Marcus <Marcus.Fessele@dm.de>
2018-07-30 16:09:32 +0200

    COUP-625 use default naming convention with dots for influx

--- src/main/java/de/dm/infrastructure/metrics/binder/GenericClassMethodMetrics.java
+++ src/main/java/de/dm/infrastructure/metrics/binder/GenericClassMethodMetrics.java
@@ -2,6 +2,7 @@ package de.dm.infrastructure.metrics.binder;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.MeterBinder;
+import io.micrometer.core.instrument.config.NamingConvention;
 
 public class GenericClassMethodMetrics implements MeterBinder {
 
@@ -11,6 +12,7 @@ public class GenericClassMethodMetrics implements MeterBinder {
     public void bindTo(MeterRegistry registry) {
 
         this.registry = registry;
+        this.registry.config().namingConvention(NamingConvention.dot);
     }
 
     public MeterRegistry getRegistry() {
