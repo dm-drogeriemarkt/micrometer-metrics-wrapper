f22264e00f7adb49f0114dab06f3a6a29e1522c1
Fessele, Marcus <Marcus.Fessele@dm.de>
2018-07-31 14:28:14 +0200

    COUP-625: enable properties for library

--- src/main/java/de/dm/infrastructure/metrics/MicrometerMetricsAutoConfiguration.java
+++ src/main/java/de/dm/infrastructure/metrics/MicrometerMetricsAutoConfiguration.java
@@ -6,8 +6,10 @@ import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegi
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource(value = "classpath:application.properties")
 @AutoConfigureAfter(CompositeMeterRegistryAutoConfiguration.class)
 public class MicrometerMetricsAutoConfiguration {
 
--- src/test/java/de/dm/infrastructure/metrics/annotation/aop/ApplicationIntegrationTest.java
+++ src/test/java/de/dm/infrastructure/metrics/annotation/aop/ApplicationIntegrationTest.java
@@ -2,15 +2,19 @@ package de.dm.infrastructure.metrics.annotation.aop;
 
 import de.dm.infrastructure.metrics.testfixtures.InterfaceWithMetricAnnotation;
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
+import io.micrometer.statsd.StatsdFlavor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.statsd.StatsdProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 @SpringBootTest
@@ -23,6 +27,10 @@ public class ApplicationIntegrationTest {
     @Autowired
     private CompositeMeterRegistry compositeMeterRegistry;
 
+
+    @Autowired
+    private StatsdProperties statsdProperties;
+
     @Test
     public void testIfMemoryMetricClassIsAttachedToRegistry() {
         this.interfaceWithMetricAnnotation.interfaceMethod();
@@ -34,5 +42,10 @@ public class ApplicationIntegrationTest {
             }
         });
         assertTrue(isIncluded.get());
+    }
+
+    @Test
+    public void testThatStatsdFlavorIsTelegraf() {
+        assertThat(statsdProperties.getFlavor(), is(StatsdFlavor.TELEGRAF));
     }
 }
