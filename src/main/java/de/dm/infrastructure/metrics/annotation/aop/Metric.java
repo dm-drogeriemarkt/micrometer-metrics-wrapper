1ef66a4ed967754f7fd085294a202b90b950c0c0
Fuchs, Daniel <Daniel.Fuchs@dm.de>
2018-08-09 11:05:35 +0200

    SpEL Comments überarbeiten

--- src/main/java/de/dm/infrastructure/metrics/annotation/aop/Metric.java
+++ src/main/java/de/dm/infrastructure/metrics/annotation/aop/Metric.java
@@ -27,19 +27,6 @@ public @interface Metric {
      * If left empty (the default) the class's simple name (as returned by {@link Class#getSimpleName()}) will be used.
      * <p>If used at the method level, metrics will use this name as a suffix.
      * If left empty (the default) the method's name (as returned by {@link Method#getName()}) will be used.
-     * <p>Supports SpEL expressions enclosed in {@code #{}}.<br>
-     * The SpEL expression evaluates again a dedicated context that provides the
-     * following meta-data:
-     * <ul>
-     * <li>{@code #result} for a reference to the result of the method invocation.</li>
-     * <li>{@code #root.method} and {@code #root.target} for a
-     * reference to the {@link Method method} and target object respectively.</li>
-     * <li>Shortcuts for the method name ({@code #root.methodName}) and target class
-     * ({@code #root.targetClass}) are also available.
-     * <li>Method arguments can be accessed by index. For instance the second argument
-     * can be access via {@code #root.args[1]}, {@code #p1} or {@code #a1}. Arguments
-     * can also be accessed by name if that information is available.</li>
-     * </ul>
      * <p>This attribute may <strong>not</strong> be used in conjunction with
      * {@link #value}, but it may be used <em>instead</em> of {@link #value}.
      */
