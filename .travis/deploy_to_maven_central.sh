b82ed33820ec053965a1264907d0c384f1d87d45
Dominik Johs <Dominik.Johs@dm.de>
2018-09-17 11:01:35 +0200

    fix deploy to maven-central

--- .travis/deploy_to_maven_central.sh
+++ .travis/deploy_to_maven_central.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] || [ ! -z "$TRAVIS_TAG" ]; then
-    openssl aes-256-cbc -K $encrypted_8edb3a2488b9_key -iv $encrypted_8edb3a2488b9_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_773c0f19f56c_key -iv $encrypted_773c0f19f56c_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
     gpg --fast-import .travis/codesigning.asc
 
     if [ ! -z "$TRAVIS_TAG" ]
