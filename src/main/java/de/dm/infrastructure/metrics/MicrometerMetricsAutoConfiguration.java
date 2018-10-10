f798fdc3c06002e1c87a471b1305ac4050262e3a
Dominik Johs <Dominik.Johs@dm.de>
2018-10-10 14:40:37 +0200

    Remove PropertySource Annotation closes #1

--- src/main/java/de/dm/infrastructure/metrics/MicrometerMetricsAutoConfiguration.java
+++ src/main/java/de/dm/infrastructure/metrics/MicrometerMetricsAutoConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource(value = "classpath:application.properties")
 @AutoConfigureAfter(CompositeMeterRegistryAutoConfiguration.class)
 public class MicrometerMetricsAutoConfiguration {
 
