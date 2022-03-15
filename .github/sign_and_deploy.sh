df0b8c12891b923baa39de39ce9d70c109c584eb
Dominik Johs <dominik.johs@dm.de>
2022-03-15 12:10:56 +0100

    fix deploy script

--- .github/sign_and_deploy.sh
+++ .github/sign_and_deploy.sh
@@ -5,7 +5,7 @@ echo $GPG_KEY_BASE64 | base64 -d  > ${KEY_FILE}
 gpg --passphrase "${GPG_PASSPHRASE}" --batch --yes --fast-import ${KEY_FILE}
 
 if [[ "${REF_TYPE}" == "tag" ]]; then
-    mvn --settings .github/mvnsettings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=REF_NAME 1>/dev/null 2>/dev/null
+    mvn --settings .github/mvnsettings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=${REF_NAME} 1>/dev/null 2>/dev/null
     mvn deploy -P sign,build-extras --settings .github/mvnsettings.xml -DskipTests=true -B -U
     SUCCESS=$?
 else
